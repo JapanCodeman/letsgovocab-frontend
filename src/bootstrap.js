a6f93dbf32e52e60bc793b67341dda25cc123d82
JapanCodeman <ryansurdick@MacBook-Pro.local>
2022-06-14 06:40:53 +0900

    added .babelrc

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,54 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./components/app";
-
-// import "./style/main.scss";
-
-// function main() {
-//   ReactDOM.render(
-//         <App/>,
-//     document.querySelector(".app-wrapper")
-//   );
-// }
-
-// document.addEventListener("DOMContentLoaded", main);
-
-
-   
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { BrowserRouter } from "react-router";
-// import App from "./components/app";
-
-
-// import "./style/main.scss";
-
-// function main() {
-//   ReactDOM.render(
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>,
-//     document.querySelector(".app-wrapper")
-//   );
-// }
-
-// document.addEventListener("DOMContentLoaded", main);
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import {Router} from "react-router";
-// import App from './components/app';
-// import history from "./history";
-
-// ReactDOM.render(
-//   <Router history={history}>
-//     <App />
-//   </Router>,
-//   document.getElementById('root')
-// );
-
-// document.addEventListener("DOMContentLoaded", main);
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -73,3 +22,28 @@ function main() {
 }
 
 document.addEventListener("DOMContentLoaded", main);
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { Provider } from "react-redux";
+// import { createStore, applyMiddleware } from "redux";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./components/app";
+// import reducers from "./reducers";
+
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+// import "./style/main.scss";
+
+// function main() {
+//   ReactDOM.render(
+//     <Provider store={createStoreWithMiddleware(reducers)}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>,
+//     document.querySelector(".app-wrapper")
+//   );
+// }
+
+// document.addEventListener("DOMContentLoaded", main);
